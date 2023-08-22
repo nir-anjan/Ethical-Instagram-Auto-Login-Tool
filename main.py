9305f21997a7b8430bb0d38e2c8054639e57fa97
Niranajn P Shaji <108530839+nir-anjan@users.noreply.github.com>
2023-08-22 23:16:32 +0530

    Update and rename debugging_stage.py to main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-
+import pyautogui
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
-
 from selenium.webdriver.common.by import By
 import time
+
+
 driver = webdriver.Chrome()
-wait=WebDriverWait(driver,20)
+wait=WebDriverWait(driver,200)
+
+
 
 def login(user,pw):
     uesername_elt=wait.until(expected_conditions.presence_of_element_located((By.NAME, 'username')))
@@ -22,52 +25,46 @@ def login(user,pw):
     pwd_elt.send_keys(pw)
     pwd_elt.send_keys(Keys.RETURN)
 
-    #time.sleep(5)
+    if (wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "_ab2z")))):
+        print("Sorry, your password was incorrect. Please double-check your password.")
+        return
+
+    #save your login details pop up
     wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,"_ac8f"))).click()
     
     print("hehe")
+    #notification permission pop up
     wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,"_a9--._a9_1"))).click()
-    #driver.find_element(By.CLASS_NAME,"_a9-- _a9_1").click()
+   
     return
 
-def messaging():
-    driver.find_element(By.CLASS_NAME,"x4k7w5x x1h91t0o x1h9r5lt x1jfb8zj xv2umb2 x1beo9mf xaigb6o x12ejxvf x3igimt xarpa2k xedcshv x1lytzrv x1t2pt76 x7ja8zs x1qrby5j").click()
-    
-    return
 
 def logout():
     more="xl5mz7h"
-    wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,'//*[@id="mount_0_0_ow"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[3]/span/div/a/div'))).click()
-
-    
-    #wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,))).click()
-    #profile x1n2onr6
-
+    wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME,more))).click() 
+  
+    for i in range(0,7):
+        pyautogui.press('tab')   
+        time.sleep(1)
+    pyautogui.press('enter')
     return
 
 
-
 #open a tab with that link
-driver.get("https://www.instagram.com/")  
-driver.maximize_window()    
+driver.get("https://www.instagram.com/") 
+
+#maximize the window
+driver.maximize_window()
 
 
-
-
-
-uname = "___ob__i__to___"
-pwd = "newnewme@2023"
+uname = input("Enter the your Username: ")
+pwd = input("Enter the your Password: ")
 login(uname,pwd)
-print("gfahgdfahsgdha")
 
-#messaging()
-time.sleep(3)
-logout()
-ch=int(input("enter choice: "))
+ch=int(input("If you want to logout enter 1: "))
 if ch==1:
+    logout()
+    print("Closing Browser....")
+    time.sleep(3)
     driver.close()
-    print("successfuly completed! ")
-else:
-    time.sleep(120)
-    driver.close()
-    print("successfuly completed! ")
+    print("Logout successful! ")
