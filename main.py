6cd7d73072a99546a3a70ce396fb7c7f67918c7d
Niranajn P Shaji <108530839+nir-anjan@users.noreply.github.com>
2023-09-01 23:13:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 import time
 
-#set chrome as browser
+
 driver = webdriver.Chrome()
 wait=WebDriverWait(driver,200)
 
@@ -24,7 +24,7 @@ def login(user,pw):
     pwd_elt.clear()
     pwd_elt.send_keys(pw)
     pwd_elt.send_keys(Keys.RETURN)
-
+    
     if (wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "_ab2z")))):
         print("Sorry, your password was incorrect. Please double-check your password.")
         return
@@ -47,21 +47,22 @@ def logout():
         pyautogui.press('tab')   
         time.sleep(1)
     pyautogui.press('enter')
+
     return
 
 
-#main
+
+
 uname = input("Enter the your Username: ")
 pwd = input("Enter the your Password: ")
-
 #open a tab with that link
 driver.get("https://www.instagram.com/") 
 
 #maximize the window
 driver.maximize_window()
 
-
 login(uname,pwd)
+
 
 ch=int(input("If you want to logout enter 1: "))
 if ch==1:
